f9dc9b8ea661ff7a60475d4d20e45845d01ff4bc
yayner <yayner2002@gmail.com>
2022-09-27 23:56:36 +0300

    Implement TrimmerDecorator

--- decorator.rb
+++ decorator.rb
@@ -1,18 +1,33 @@
 require './nameable'
 
 class Decorator < Nameable # glocal decorator class
+  attr_accessor :nameable # getter and setter for nameable instance var
 
   def initialize(nameable) # constructor
     super()
     @nameable = nameable
   end
 
-  attr_accessor :nameable # getter and setter for nameable instance var
-
-  def correct_name
+  def correct_name # the decorator delegates all work to the nameable
     @nameable.correct_name
+  end 
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
   end
 
   
+end
+
+class TrimmerDecorator < Decorator
+def correct_name
+  if @nameable.correct_name.length > 10
+    @nameable.correct_name.slice(0..10)
+  end
+  else
+    @nameable.correct_name
   
+end
 end
