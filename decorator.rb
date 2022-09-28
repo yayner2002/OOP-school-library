a830e0a594dac134fe3fb212121b0a2206e259e2
yayner <yayner2002@gmail.com>
2022-09-28 09:30:14 +0300

    Finalize the decorator design pattern

--- decorator.rb
+++ decorator.rb
@@ -1,33 +1,33 @@
 require './nameable'
 
-class Decorator < Nameable # glocal decorator class
+# global decorator class
+class Decorator < Nameable
   attr_accessor :nameable # getter and setter for nameable instance var
 
-  def initialize(nameable) # constructor
+  # constructor
+  def initialize(nameable)
     super()
     @nameable = nameable
   end
 
-  def correct_name # the decorator delegates all work to the nameable
+  # the decorator delegates all work to the nameable
+  def correct_name
     @nameable.correct_name
-  end 
+  end
 end
 
 class CapitalizeDecorator < Decorator
   def correct_name
     @nameable.correct_name.capitalize
   end
-
-  
 end
 
 class TrimmerDecorator < Decorator
-def correct_name
-  if @nameable.correct_name.length > 10
-    @nameable.correct_name.slice(0..10)
+  def correct_name
+    if @nameable.correct_name.length > 10
+      @nameable.correct_name.slice(0..10)
+    else
+      @nameable.correct_name
+    end
   end
-  else
-    @nameable.correct_name
-  
-end
 end
