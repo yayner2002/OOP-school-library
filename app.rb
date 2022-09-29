2bccea4c6b89798eaceb02166b09e833cbc1e7e5
yayner <yayner2002@gmail.com>
2022-09-29 23:16:52 +0300

    Implement list all books

--- app.rb
+++ app.rb
@@ -11,6 +11,17 @@ class Apps
     @peole = []
     @books = []
   end
+
+  def list_books
+    if @books.empty?
+      puts 'No Available books in the store'
+      return
+    end
+    @books.map do |book|
+      puts "Title: \"#{book.title}\", Author: #{book.author}"
+    end
+  end
+
   
 end
 
