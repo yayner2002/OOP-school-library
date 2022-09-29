59b70aea9710ecaccbb4270e94c9eedb08cd66d0
yayner <yayner2002@gmail.com>
2022-09-29 14:53:55 +0300

    Add has many relationship to book nad person

--- person.rb
+++ person.rb
@@ -1,13 +1,15 @@
 require './nameable'
 
 class Person < Nameable
+  attr_reader :id, :rental # getter
+  attr_accessor :name, :age # getter and setter
+
   # A constructor initialize with init parameters that any new instance of class should have
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-
     @rental = []
     super()
   end
@@ -16,15 +18,16 @@ class Person < Nameable
     @name
   end
 
-  attr_reader :id, rental # getter
-  attr_accessor :name, :age # getter and setter
+  def add_rental(date, book)
+    Rental.new(date, book, self)
+  end
 
   def of_age?
     @age >= 18
   end
 
   def can_use_services?
-    of_age? || @parent_permission == true
+    of_age? || @parent_permission
   end
   private :of_age?
 end
