6b8e7451e1b49166ce5122f595f74c951e2165bc
yayner <yayner2002@gmail.com>
2022-09-29 23:08:46 +0300

    Create app class and include files

---
+++ app.rb
@@ -0,0 +1,16 @@
+require './person'
+require './teacher'
+require './student'
+require './book'
+require './rental'
+require 'date'
+
+class Apps
+  def initialize
+    @rentals = []
+    @peole = []
+    @books = []
+  end
+  
+end
+
