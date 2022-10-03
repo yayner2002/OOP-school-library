dec2c58b6ea42e8e3514f1b9ad8447a55362d70d
yayner <yayner2002@gmail.com>
2022-10-03 15:37:47 +0300

    Create the main menu method

--- main.rb
+++ main.rb
@@ -4,8 +4,20 @@ class Main
   def initialize
     @apps = Apps.new
   end
-  
 
+
+  def main_menu
+    puts "
+    1 - list all books.
+    2 - list all people.
+    3 - create a person.
+    4 - create a book.
+    5 - create a rental.
+    6 - list all rental for a given person id.
+    7 - Exit"
+  end
+    
+  
 
 end
 
